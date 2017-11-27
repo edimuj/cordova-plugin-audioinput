e850d9e32ac08a331398e352bba4f85b31d9df48
Robert Fromont <robert@fromont.net.nz>
2017-11-27 17:52:45 -0300

    Get fileUrl usage working on Chrome by ensuring URL is of the form cdvfile://localhost/...

--- src/browser/AudioInputCaptureProxy.js
+++ src/browser/AudioInputCaptureProxy.js
@@ -89,6 +89,8 @@ function start(success, error, opts) {
     format = opts[3] || format;
     audioSourceType = opts[4] || audioSourceType;
     fileUrl = opts[5] || fileUrl;
+    // the URL must be converted to a cdvfile:... URL to ensure it's readable from the outside
+    fileUrl = fileUrl.replace("filesystem:file:///", "cdvfile://localhost/");
     console.log("AudioInputCaptureProxy: start - fileUrl: " + fileUrl);
 
     if (!audioRecorder) {
